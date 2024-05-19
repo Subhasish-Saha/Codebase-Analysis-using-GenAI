291dc5112cc50a0bcdd97d24cbf7e2a7711a9897
Subhasish Saha <subhasishsaha007@gmail.com>
2024-05-19 19:32:14 +0530

    Added : package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Code_Analyser',
+    version= '0.0.1',
+    author= 'Subhasish Saha',
+    author_email= 'subhasishsaha007@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
